a5d24a98ac7e4b41cb82b8cb4d7e69232342899b
dyl-green <drosser978@gmail.com>
2026-06-21 12:30:02 -0400

    ADD: playwright will now click on carousel

--- scraper.py
+++ scraper.py
@@ -3,6 +3,17 @@ from playwright.sync_api import sync_playwright
 import json
 
 
+def extract_item_info(soup):
+
+    title = soup.find('h1', class_="web_ui__Text__text web_ui__Text__title web_ui__Text__left").text
+    print(f"Title: {title}")
+
+    description = soup.find('span', class_="web_ui__Text__text web_ui__Text__body web_ui__Text__left web_ui__Text__format").text
+    print(f"Description: {description}")
+
+    return title, description
+
+
 url = "https://www.vinted.com/member/256710934"
 
 url = "https://www.vinted.com/items/9176775363-vintage-nike-track-pants-large-navy-blue-baggy-lined-y2k-grunge-skater"
@@ -13,14 +24,16 @@ with sync_playwright() as p:
     page.goto(url)
 
     page.wait_for_timeout(2000) # Give the page time to load
+    # Much shorter, safer, and less prone to breaking
+    # Direct click using your exact selector path
+    
+    page.locator("html body.v_inter_b0f5c119-module__kyvDTG__className div.next-page div.standard-layout.is-wide main.site.u-background-white div div.site-content div.container div.row.u-position-relative div#content.grid12 div.body-content main.item-information div#content.item-page-content section.item-main section.item-photos__container div.item-photos figure.item-description.u-flexbox.item-photo.item-photo--5 button.item-thumbnail").click()
+    
+    page.wait_for_timeout(2000) # Give the page time to load
 
     html = page.content()
     browser.close()
 
 soup = BeautifulSoup(html, 'html.parser')
 
-title = soup.find('h1', class_="web_ui__Text__text web_ui__Text__title web_ui__Text__left").text
-print(f"Title: {title}")
-
-description = soup.find('span', class_="web_ui__Text__text web_ui__Text__body web_ui__Text__left web_ui__Text__format").text
-print(f"Description: {description}")
+extract_item_info(soup)
