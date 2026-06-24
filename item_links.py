db174da7ed5c62a4ef74355501f067a1a6260ffb
dyl-green <drosser978@gmail.com>
2026-06-24 19:36:31 -0400

    Script will now print out all links of the members items

--- item_links.py
+++ item_links.py
@@ -2,11 +2,17 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import json
 
-url = "https://www.vinted.com/member/256710934"
+
+def alter_title(title):
+    return title.split(",")[0].strip().lower().replace(" ", "-").replace("'", "").replace("%", "")
+
+member_url = "https://www.vinted.com/member/256710934"
+member_item_urls = []
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto(url)
+    page.goto(member_url)
 
     page.wait_for_timeout(2000) # Give the page time to load
     # Scoll down to load all items, I want to fins a better way fpr this eventually
@@ -31,15 +37,13 @@ with sync_playwright() as p:
     # Find all items using <a> tags & class selector
     items = soup.find_all('a', class_="new-item-box__overlay new-item-box__overlay--clickable")
 
-
-    item_urls = [item['title'][0] for item in items]
-    print(item_urls)
-
     # Concatonate the names of the items for the url string 
+    item_urls = ["www.vinted.com" + item['href'] + "/" + alter_title(item['title']) for item in items]
+    member_item_urls.extend(item_urls)
 
+    print(item_urls)
 
 
     browser.close()
 
-soup = BeautifulSoup(html, 'html.parser')
-
+print(member_item_urls)
